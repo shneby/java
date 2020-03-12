d7123ea483f11ae329eee03ebcfde86c7a5bbaf8
shneby@gmail.com <omri.cohen@cybereason.com>
2020-03-12 14:48:32 +0200

    sql

---
+++ classexercises/src/com/sqlwork/SqlApp.java
@@ -0,0 +1,26 @@
+package com.sqlwork;
+
+import java.sql.*;
+
+public class SqlApp {
+    public static void main(String[] args) {
+        try {
+            String url = "jdbc:mysql://127.0.0.1:3306/classicmodels";
+            String user = "root";
+            String password = "zubur123";
+            String[] flags = {"useSSL=false", "autoReconnect=true"};
+            String connStr = String.format("%s?%s", url, String.join("&", flags));
+            Connection conn = DriverManager.getConnection(connStr, user, password);
+
+            Statement stmt = conn.createStatement();
+            ResultSet resultSet = stmt.executeQuery("SELECT * from employees");
+
+            while(resultSet.next()){
+                System.out.println(resultSet.getString("name"));
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
