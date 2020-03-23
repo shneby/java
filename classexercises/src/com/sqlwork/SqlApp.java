5198a981371a22b028e069b03a16e0979ae4009c
shneby <shneby@gmail.com>
2020-03-23 17:10:07 +0200

    sqling

--- classexercises/src/com/sqlwork/SqlApp.java
+++ classexercises/src/com/sqlwork/SqlApp.java
@@ -4,21 +4,30 @@ import java.sql.*;
 
 public class SqlApp {
     public static void main(String[] args) {
-        try {
-            String url = "jdbc:mysql://127.0.0.1:3306/classicmodels";
-            String user = "root";
-            String password = "zubur123";
-            String[] flags = {"useSSL=false", "autoReconnect=true"};
-            String connStr = String.format("%s?%s", url, String.join("&", flags));
-            Connection conn = DriverManager.getConnection(connStr, user, password);
 
-            Statement stmt = conn.createStatement();
-            ResultSet resultSet = stmt.executeQuery("SELECT * from employees");
+        String url = "jdbc:mysql://127.0.0.1:3306/classicmodels";
+        String[] flags = {"useSSL=false", "autoReconnect=true", "serverTimezone=UTC"};
+        String connStr = String.format("%s?%s", url, String.join("&", flags));
+        String queryUpdate = "update `employees` SET `lastName` = ? where `employeeNumber` = ?";
+        String querySelect = "select e.*, o.city from employees as e left join offices o on e.officeCode = o.officeCode";
+        try (
+                Connection conn = DriverManager.getConnection(connStr, "root", "zubur123");
+                PreparedStatement stmtSelect = conn.prepareStatement(querySelect);
+                PreparedStatement stmtUpdate = conn.prepareStatement(queryUpdate)
+        ){
+            stmtUpdate.setString(1, "Tahat");
+            stmtUpdate.setInt(2, 1002);
 
+            System.out.println(stmtUpdate.executeUpdate());
+            ResultSet resultSet = stmtSelect.executeQuery();
             while(resultSet.next()){
-                System.out.println(resultSet.getString("lastName"));
-            }
 
+                int employeeNumber = resultSet.getInt("employeeNumber");
+                String lastName = resultSet.getString("lastName");
+                String city = resultSet.getString("city");
+
+                System.out.println(employeeNumber + " " + lastName + " " + city);
+            }
         } catch (SQLException e) {
             e.printStackTrace();
         }
